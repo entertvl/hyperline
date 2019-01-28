836e5ba62b371f9ecbbb9f9f94fba4d5a609d443
Tomoya Taniguchi <tmy.mdw.force@gmail.com>
2019-01-28 11:43:17 +0900

    remove comments

--- src/lib/plugins/index.js
+++ src/lib/plugins/index.js
@@ -1,12 +1,9 @@
 import hostname from './hostname'
 import ip from './ip'
 import memory from './memory'
-// Import Uptime from './uptime'
 import cpu from './cpu'
 import network from './network'
 import battery from './battery'
-// Import Time from './time'
-// Import Docker from './docker'
-import spotify from './spotify'
+import time from './time'
 
-export default [hostname, ip, memory, battery, cpu, network, spotify]
+export default [hostname, ip, memory, battery, cpu, network, time]
